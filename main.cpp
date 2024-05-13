ec6c09ba55e0616575fcc1d8c6d9aa78879f0408
Ismael Bejarano <ismaelbej@gmail.com>
2024-05-13 04:33:17 +0000

    Add keccak hashing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+#include "./keccak-tiny.h"
 #include <iostream>
 #include <cstring>
 #include <secp256k1.h>
 #include <secp256k1_recovery.h>
+// #include <openssl/evp.h>
+// #include <openssl/core_names.h>
 
 // Helper function to convert hex string to byte array.
 int hex_to_bytes(unsigned char *byte_array, const char *hex_string, size_t byte_array_max_len) {
@@ -13,23 +16,13 @@ int hex_to_bytes(unsigned char *byte_array, const char *hex_string, size_t byte_
     return 1;
 }
 
-int main() {
+int sign_message(unsigned char private_key[], unsigned char message_hash[]) {
     secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
-    unsigned char private_key[32];
-    unsigned char message_hash[32]; // The hash of your message.
-
-    // Dummy private key and message hash (normally you would compute the hash of your message)
-    hex_to_bytes(private_key, "0101010101010101010101010101010101010101010101010101010101010101", sizeof(private_key));
-    hex_to_bytes(message_hash, "0202020202020202020202020202020202020202020202020202020202020202", sizeof(message_hash));
 
     secp256k1_ecdsa_recoverable_signature signature;
     if (!secp256k1_ecdsa_sign_recoverable(ctx, &signature, message_hash, private_key, NULL, NULL)) {
         fprintf(stderr, "Failed to sign the message.\n");
         return 1;
-    }
-    printf("Signature: ");
-    for (int i = 0; i < 64; i++) {
-        printf("%02x", signature.data[i]);
     }
 
     // Serialize the signature in compact format
@@ -37,7 +30,7 @@ int main() {
     int recid;
     secp256k1_ecdsa_recoverable_signature_serialize_compact(ctx, output, &recid, &signature);
 
-    printf("\nSignature: ");
+    printf("Signature: ");
     for (int i = 0; i < 64; i++) {
         printf("%02x", output[i]);
     }
@@ -45,4 +38,41 @@ int main() {
 
     secp256k1_context_destroy(ctx);
     return 0;
+}
+
+int hash_message(unsigned char message_hash[]) {
+    unsigned char hashed[32];
+    uint32_t length;
+
+    keccak_256(hashed, 32, message_hash, 32);
+
+    // EVP_MD_CTX* context = EVP_MD_CTX_new();
+    // auto md = EVP_get_digestbyname("KECCAK-256");
+    // const EVP_MD* algorithm = EVP_sha3_256();
+    // EVP_DigestInit_ex2(context, algorithm, nullptr);
+    // EVP_DigestUpdate(context, message_hash, 32);
+    // EVP_DigestFinal_ex(context, hashed, &length);
+    // EVP_MD_CTX_destroy(context);
+
+    printf("Hashed: ");
+    for (int i = 0; i < length; i++) {
+        printf("%02x", hashed[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+int main() {
+    unsigned char private_key[32];
+    unsigned char message_hash[32]; // The hash of your message.
+
+    // OpenSSL_add_all_digests();
+
+    // Dummy private key and message hash (normally you would compute the hash of your message)
+    hex_to_bytes(private_key, "0101010101010101010101010101010101010101010101010101010101010101", sizeof(private_key));
+    hex_to_bytes(message_hash, "0202020202020202020202020202020202020202020202020202020202020202", sizeof(message_hash));
+
+    // sign_message(private_key, message_hash);
+
+    hash_message(message_hash);
 }
